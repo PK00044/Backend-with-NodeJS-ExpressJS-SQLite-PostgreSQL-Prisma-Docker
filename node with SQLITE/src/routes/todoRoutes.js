a0bc308e8a7821c4f41339fe210b8554cf252989
Pratham Kubsad <prathamkubsad@gmail.com>
2025-08-26 01:42:26 +0530

    Added register and Login Routes

---
+++ node with SQLITE/src/routes/todoRoutes.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import db from '../db.js';
+
+const todoRoute = express.Router();
+
+todoRoute.get('/', (req, res) => {
+
+})
+
+todoRoute.post('/',(req, res) =>{
+
+})
+
+todoRoute.put('/:id', (req, res) => {
+
+})
+
+todoRoute.delete('/:id', (req, res) => {
+
+})
+
+export default todoRoute;
